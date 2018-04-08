7e4fcd16916e18009e511f2bdca5bbb028cfdd1c
wty <wtysos11@163.com>
2018-04-08 22:31:37 +0800

    Week4 branch

---
+++ Week4/Solar/Assets/moonMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moonMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (this.transform.parent.position, Vector3.up, 50 * Time.deltaTime);
+	}
+}
---
+++ Week4/project motion/Assets/movewithGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movewithGravity : MonoBehaviour {
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        Physics.gravity = new Vector3(0, -9.8f, 0);
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 20, 10);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Debug.Log(transform.position.ToString());
+	}
+}
