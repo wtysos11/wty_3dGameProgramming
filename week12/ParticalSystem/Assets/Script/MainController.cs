a88c22c85849d55685a090ec58a6d4f7dd1ed1f0
wty <wtysos11@163.com>
2018-05-25 23:26:56 +0800

    finish to flame and begin to make trigger

---
+++ week12/ParticalSystem/Assets/Script/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    GameObject fire;
+    GameObject water;
+    GameObject fireTrigger;
+	// Use this for initialization
+	void Start () {
+        fire = GameObject.FindGameObjectWithTag("flame");
+        water = GameObject.FindGameObjectWithTag("water");
+        fireTrigger = GameObject.FindGameObjectWithTag("fireTrigger");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- week12/ParticalSystem/Assets/Script/ParticalController.cs
+++ week12/ParticalSystem/Assets/Script/ParticalController.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class ParticalController : MonoBehaviour {
 
     ParticleSystem ps;
-    float myMin = 100.0f;
+    float myMin = 1.0f;
     float myMax = 1000.0f;
     float myBase = 1000.0f;
-	// Use this for initialization
-	void Start () {
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> inside = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> outside = new List<ParticleSystem.Particle>();
+
+    public GameObject XFTarget;
+
+
+    // Use this for initialization
+    void Start () {
         ps = GetComponent<ParticleSystem>();
+        XFTarget = GameObject.FindGameObjectWithTag("flame");
 	}
 	
 	// Update is called once per frame
@@ -27,4 +36,22 @@ public class ParticalController : MonoBehaviour {
         }
         em.rateOverTime = new ParticleSystem.MinMaxCurve(scale);
 	}
+
+    void OnParticleTrigger()
+    {
+        int enterNum = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int exitNum = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+        int insideNum = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Inside, inside);
+        int outsideNum = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Outside, outside);
+
+        Debug.Log("enter Num:" + enterNum);
+        Debug.Log("exit Num:" + exitNum);
+        Debug.Log("inside Num:" + insideNum);
+        Debug.Log("outside Num:" + outsideNum);
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Inside, inside);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Outside, outside);
+    }
 }
