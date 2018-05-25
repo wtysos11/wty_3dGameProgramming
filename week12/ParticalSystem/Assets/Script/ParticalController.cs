6efba0a1aca401ec10703e1894930f146d7875df
wty <wtysos11@163.com>
2018-05-25 22:03:10 +0800

    finish the water hose

---
+++ week12/ParticalSystem/Assets/Script/ParticalController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticalController : MonoBehaviour {
+
+    ParticleSystem ps;
+    float myMin = 100.0f;
+    float myMax = 1000.0f;
+    float myBase = 1000.0f;
+	// Use this for initialization
+	void Start () {
+        ps = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var em = ps.emission;
+        float scale = em.rateOverTime.constantMax;
+        if (Input.GetAxis("Mouse ScrollWheel")!=0)
+        {
+            scale += (float)Input.GetAxis("Mouse ScrollWheel") * myBase;
+            if (scale < myMin)
+                scale = myMin;
+            else if (scale > myMax)
+                scale = myMax;
+        }
+        em.rateOverTime = new ParticleSystem.MinMaxCurve(scale);
+	}
+}
